dee8105b42f45c36b4268bd49a978c0dc4b76ee3
tihig <vnissila@cs.helsinki.fi>
2017-01-26 09:52:26 +0200

    First two steps done
    From chapter 2, stiull porblem with dplyr (?) library

---
+++ data/create_learning2014.R
@@ -0,0 +1,38 @@
+# tihig, 26.1.2017, this is first script of chapter 2
+
+# http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt 
+
+lrn14 <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=TRUE)
+
+# structure looks pretty messy, there's loads of shortnings like SU01 or Aa. 
+#In the end there's some readable columns, like Age and Gender 
+structure(lrn14)
+
+#has two numbers: 183 and 60 (objects and variables)
+dim(lrn14)
+
+# something wrong with installations...
+install(dplyr)
+.libPaths()
+library(dplyr)
+
+# questions related to deep, surface and strategic learning
+deep_questions <- c("D03", "D11", "D19", "D27", "D07", "D14", "D22", "D30","D07","D14","D22","D30")
+surface_questions <- c("SU02","SU10","SU18","SU26", "SU05","SU13","SU21","SU29","SU08","SU16","SU24","SU32")
+strategic_questions <- c("ST01","ST09","ST17","ST25","ST04","ST12","ST20","ST28")
+
+
+# select the columns related to deep learning and create column 'deep' by averaging
+deep_columns <- select(lrn14, one_of(deep_questions))
+lrn14$deep <- rowMeans(deep_columns)
+
+# select the columns related to surface learning and create column 'surf' by averaging
+surface_columns <- select(lrn14, one_of(surface_questions))
+lrn14$surf <- rowMeans(surface_columns)
+
+# select the columns related to strategic learning and create column 'stra' by averaging
+strategic_columns <- select(lrn14, one_of(strategic_questions))
+lrn14$stra <- rowMeans(strategic_columns)
+
+
+
