8665be38982edd53eaf29f6e4cfc74200f934349
tihig <vnissila@cs.helsinki.fi>
2017-02-17 10:55:06 +0200

    Chapter 4: data wrangling complete

--- data/create_human.R
+++ data/create_human.R
@@ -13,7 +13,7 @@ summary(gii)
 
 colnames(hd)
 
-library(plyr)
+#named them with my own names
 new_hd <- c("Rank", "Country", "HDI", "eYearsEdu", "expectedEDU", "meanEDU" , "GNI", "GNImHDI" )
 new_gii <- c("Rank", "Country", "GII", "materMort", "adBirth", "reprParl", "secEdF", "secEdM", "labourF", "labourM")
 
@@ -21,3 +21,19 @@ colnames(hd) <-new_hd
 colnames(gii) <-new_gii  
 colnames(hd)
 colnames(gii)
+
+
+library(dplyr); library(ggplot2)
+
+gii <- mutate(gii, eduFM = (secEdF / secEdM))
+gii <- mutate(gii, labourFM = (labourF / labourM))
+
+
+?inner_join
+
+human <- inner_join(hd, gii, by = c("Country"), suffix = c(".hd", ".gii"))
+colnames(human)
+dim(human)
+?write.csv
+
+write.csv(human, file="human.csv", row.names= FALSE)
